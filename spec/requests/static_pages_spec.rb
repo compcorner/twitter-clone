d459e8c00f5a2d79a615c542dc54f9f4144d0463
Dionysis Zindros <dionyziz@gmail.com>
2013-11-17 17:00:20 -0800

    AJAX and regular follow/unfollow buttons implementation

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -33,8 +33,8 @@ describe "StaticPages" do
           visit root_path
         end
 
-        it { should have_link("0 following", href: following_user_path(user)) }
-        it { should have_link("1 followers", href: followers_user_path(user)) }
+        it { should have_link("0 Following", href: following_user_path(user)) }
+        it { should have_link("1 Followers", href: followers_user_path(user)) }
       end
     end
   end
